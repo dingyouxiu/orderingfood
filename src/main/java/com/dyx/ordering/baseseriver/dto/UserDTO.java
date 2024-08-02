09aaafbd8870bfe1127e6e215b199ff53cefe26c
dinghaipeng <dinghaipeng@epm.com>
2024-08-02 15:38:18 +0800

    小程序框架构造

--- src/main/java/com/dyx/ordering/baseseriver/dto/UserDTO.java
+++ src/main/java/com/dyx/ordering/baseseriver/dto/UserDTO.java
@@ -2,7 +2,9 @@ package com.dyx.ordering.baseseriver.dto;
 
 import com.dyx.ordering.baseseriver.entity.UserEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class UserDTO extends UserEntity {
 }
